4970bdb8abee5d8d54450ac33e9a4d622933449f
Zach Overhulser <zoverhulser@gmail.com>
2019-05-17 18:06:06 +0000

    Gets project by id

--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -8,4 +8,16 @@ router.get("/", (req, res) => {
     }).catch(err => handleRes(res, 500, {error: "There was an error retrieving the projects"}))
 });
 
+router.get("/:id", (req, res) => {
+    db("projects").where({id: req.params.id}).first()
+      .then(project => {
+	  if(project) {
+	      handleRes(res, 200, {success: true, project});
+	  }
+	  if(!project) {
+	      handleRes(res, 404, {success: false, message: `Project with id of ${req.params.id} not found`})
+	  }
+      }).catch(err => handleRes(res, 500, err))
+})
+
 module.exports = router;
